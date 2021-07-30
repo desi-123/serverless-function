3648bb25bb0b016e410cc3293352b38759104c6d
Desalegn Wagaw <desiemeng21m@gmail.com>
2021-07-30 04:34:00 -0700

    example 3 complete

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -7,12 +7,12 @@ const fetchData = async () => {
             const {image: {url}, name, price} = product
             return `
                 <article class="product">
-        <img src="${url}" alt="${name}" />
-        <div class="info">
-        <h5>${name}</h5>
-        <h5 class="price">${price}</h5>
-        </div>
-        </article>
+                <img src="${url}" alt="${name}" />
+                <div class="info">
+                    <h5>${name}</h5>
+                    <h5 class="price">${price}</h5>
+                </div>
+                </article>
             `
         })
         .join('')
